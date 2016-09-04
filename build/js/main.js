e93ab6937120f8fc297ba99bd1bb7b88562d1d2f
Aleksandr Poleliuk <cawkabss@gmail.com>
2016-09-04 11:58:06 +0300

    fixed bugs

--- build/js/main.js
+++ build/js/main.js
@@ -1,221 +1,119 @@
 $(document).ready(function () {
-    var $sliderList = $('.works-slider-list'),
-        $next = $('.slider-control-next'),
-        $prev = $('.slider-control-prev'),
-        $sliderItem = $('.works-slider-i'),
-        currentSlide = 0,
-        $sliderWidth = $('.works-slider').width();
+
+    var slider = {
+        autoPlay: '',
+        sliderItemWidth: '',
+        sliderItemMargin: '',
+        $sliderList : $('.works-slider-list'),
+        $next : $('.slider-control-next'),
+        $prev : $('.slider-control-prev'),
+        $sliderItem : $('.works-slider-i'),
+        currentSlide : 0,
+        $sliderWidth : $('.works-slider').width()
+
+    };
 
     function response (){
-        var $sliderList = $('.works-slider-list'),
-            $next = $('.slider-control-next'),
-            $prev = $('.slider-control-prev'),
-            $sliderItem = $('.works-slider-i'),
-            currentSlide = 0,
-            $sliderWidth = $('.works-slider').width();
+        slider.$sliderList = $('.works-slider-list');
+        slider.$next = $('.slider-control-next');
+        slider.$prev = $('.slider-control-prev');
+        slider.$sliderItem = $('.works-slider-i');
+        slider.currentSlide = 0;
+        slider.$sliderWidth = $('.works-slider').width();
+        $.each(slider.$sliderItem, function(){
+           $(this).removeClass('hidden').removeClass('active').css({
+               'width': slider.sliderItemWidth,
+               'margin': slider.sliderItemMargin
+           });
+        });
+        slider.$sliderItem.first().addClass('active');
         if($(window).width() > 992){
+            clearInterval(slider.autoPlay);
+            slider.sliderItemWidth = slider.$sliderWidth * 0.4 + 'px';
+            slider.sliderItemMargin = '0 ' + slider.$sliderWidth * 0.05 + 'px';
 
-            $sliderItem.css({
-                'width': $sliderWidth * 0.4 + 'px',
-                'margin': '0 ' + $sliderWidth * 0.05 + 'px'
+            slider.$sliderItem.css({
+                'width': slider.$sliderWidth * 0.4 + 'px',
+                'margin': '0 ' + slider.$sliderWidth * 0.05 + 'px'
             });
-            $($next).css('display', 'block');
-            $($prev).css('display', 'block');
+            $(slider.$next).css('display', 'block');
+            $(slider.$prev).css('display', 'block');
 
         }
-        else if($(window).width() > 769 && $(window).width() < 991){
+            else if($(window).width() < 992){
+            clearInterval(slider.autoPlay);
+            slider.autoPlay = setInterval(slideShowTablet2, 2000);
 
-            $sliderItem.css({
-                'width': $sliderWidth * 0.6 + 'px',
-                'margin': '0 ' + $sliderWidth * 0.1 + 'px'
-            });
-            $next.css('display', 'none');
-            $prev.css('display', 'none');
+                slider.sliderItemWidth = slider.$sliderWidth * 0.6 + 'px';
+                slider.sliderItemMargin = '0 ' + slider.$sliderWidth * 0.1 + 'px';
+
+            slider.$next.css('display', 'none');
+            slider.$prev.css('display', 'none');
 
 
-            currentSlide = 0;
-            (function(){
-                $.each($sliderItem, function(){
-                    $(this).removeClass('hidden');
-                    $sliderItem.first().addClass('active')
-                })
-            })();
-            window.tabletInterval = setInterval(slideShowTablet2, 2000);
-            function slideShowTablet2() {
+             if($(window).width() < 768){
+                slider.sliderItemWidth = slider.$sliderWidth + 'px';
+                slider.sliderItemMargin = '0 15px 0 0';
 
-                if(currentSlide + 1  == $sliderList.children().length){
-                    currentSlide = 0;
-                    $.each($sliderList.children(), function(){
-                        $(this).removeClass('active').removeClass('hidden').css({
-                            'width': $sliderWidth * 0.6 + 'px',
-                            'margin': '0 ' + $sliderWidth * 0.1 + 'px'
-                        })
-                    });
-                    $sliderList.children().eq(currentSlide).addClass('active');
-
-                    return slideShowTablet2();
-                }
-
-                currentSlide++;
-                console.log(currentSlide);
-                $sliderList.children().eq(currentSlide).addClass('active').prev().addClass('hidden').removeClass('active').css({
-                    'width': 0,
-                    'margin': 0
-                });
-            }
-
-        }
-        else if($(window).width() < 768){
-
-            $sliderItem.css({
-                'width': $sliderWidth + 'px',
-                'margin': '0 15px 0 0'
-            });
-            $next.css('display', 'none');
-            $prev.css('display', 'none');
-
-            currentSlide = 0;
-            (function(){
-                $.each($sliderItem, function(){
-                    console.log($(this));
-                    $(this).removeClass('hidden');
-                    $sliderItem.first().addClass('active')
-                })
-            })();
-             window.mobileInterval = setInterval(slideShowMobile, 2000);
-            function slideShowMobile() {
-                if(currentSlide + 1  == $sliderList.children().length){
-                    currentSlide = 0;
-                    $.each($sliderList.children(), function(){
-                        $(this).removeClass('active').removeClass('hidden').css({
-                            'width': $sliderWidth + 'px',
-                            'margin': '0 15px 0 0'
-                        })
-                    });
-                    $sliderList.children().eq(currentSlide).addClass('active');
-
-                    return slideShowMobile();
-                }
-                currentSlide++;
-                console.log(currentSlide);
-                $sliderList.children().eq(currentSlide).addClass('active').prev().addClass('hidden').removeClass('active').css({
-                    'width': 0,
-                    'margin': 0
-                });
             }
         }
     }
 
 
     $(window).resize(function(){
-
-        clearInterval(ll);
-        clearInterval(kk);
-        clearInterval(window.mobileInterval);
-        clearInterval(window.tabletInterval);
-
         response();
+
     });
 
+    response();
 
+    slider.$sliderItem.css({
+        'width': slider.sliderItemWidth,
+        'margin': slider.sliderItemMargin
+    });
+    $(slider.$next).on('click', function(){
 
-        if($(window).width() > 992){
-            $sliderItem.css({
-                'width': $sliderWidth * 0.4 + 'px',
-                'margin': '0 ' + $sliderWidth * 0.05 + 'px'
-            });
-            $($next).css('display', 'block');
-            $($prev).css('display', 'block');
-
-            $($next).on('click', function(){
-
-                if(currentSlide + 1  == $sliderList.children().length){
-                    return;
-                }
-                currentSlide++;
-
-                $sliderList.children().eq(currentSlide).addClass('active').prev().addClass('hidden').removeClass('active').css({
-                    'width': 0,
-                    'margin': 0
-                });
-
-            });
-            $($prev).on('click', function(){
-
-                if(currentSlide == 0){
-                    return;
-                }
-                currentSlide--;
-                $sliderList.children().eq(currentSlide).addClass('active').removeClass('hidden').css({
-                    'width': $sliderWidth* 0.4 + 'px',
-                    'margin': '0 ' + $sliderWidth * 0.05 + 'px'
-                }).next().removeClass('active');
-
-            });
-
+        if(slider.currentSlide + 1  == slider.$sliderList.children().length){
+            return;
         }
-        else if($(window).width() >= 768 && $(window).width() <= 992){
-            $sliderItem.css({
-                'width': $sliderWidth * 0.6 + 'px',
-                'margin': '0 ' + $sliderWidth * 0.1 + 'px'
-            });
-            $next.css('display', 'none');
-            $prev.css('display', 'none');
-            var kk = setInterval(slideShowTablet2, 2000);
+        slider.currentSlide++;
 
-            function slideShowTablet2() {
-                if(currentSlide + 1  == $sliderList.children().length){
-                    currentSlide = 0;
-                    $.each($sliderList.children(), function(){
-                        $(this).removeClass('active').removeClass('hidden').css({
-                            'width': $sliderWidth * 0.6 + 'px',
-                            'margin': '0 ' + $sliderWidth * 0.1 + 'px'
-                        })
-                    });
-                    $sliderList.children().eq(currentSlide).addClass('active');
+        slider.$sliderList.children().eq(slider.currentSlide).addClass('active').prev().addClass('hidden').removeClass('active').css({
+            'width': 0,
+            'margin': 0
+        });
 
-                    return slideShowTablet2();
-                }
+    });
+    $(slider.$prev).on('click', function(){
 
-                currentSlide++;
-                $sliderList.children().eq(currentSlide).addClass('active').prev().addClass('hidden').removeClass('active').css({
-                    'width': 0,
-                    'margin': 0
+        if(slider.currentSlide == 0){
+            return;
+        }
+        slider.currentSlide--;
+        slider.$sliderList.children().eq(slider.currentSlide).addClass('active').removeClass('hidden').css({
+            'width': slider.$sliderWidth* 0.4 + 'px',
+            'margin': '0 ' + slider.$sliderWidth * 0.05 + 'px'
+        }).next().removeClass('active');
+
+    });
+
+        function slideShowTablet2() {
+            if(slider.currentSlide == slider.$sliderItem.length){
+                $.each(slider.$sliderItem, function(){
+                    $(this).removeClass('hidden').removeClass('active');
                 });
+                slider.$sliderItem.css({
+                    'width': slider.sliderItemWidth,
+                    'margin': slider.sliderItemMargin
+                });
+                slider.currentSlide = -1;
             }
 
-        }
-        else if($(window).width() < 768){
-
-            $sliderItem.css({
-                'width': $sliderWidth + 'px',
-                'margin': '0 15px 0 0'
+            slider.currentSlide++;
+            slider.$sliderList.children().eq(slider.currentSlide).addClass('active').prev().addClass('hidden').removeClass('active').css({
+                'width': 0,
+                'margin': 0
             });
-            $next.css('display', 'none');
-            $prev.css('display', 'none');
-
-            var ll = setInterval(slideShowMobile, 2000);
-            function slideShowMobile() {
-                if(currentSlide + 1  == $sliderList.children().length){
-                    currentSlide = 0;
-                    $.each($sliderList.children(), function(){
-                        $(this).removeClass('active').removeClass('hidden').css({
-                            'width': $sliderWidth + 'px',
-                            'margin': '0 15px 0 0'
-                        })
-                    });
-                    $sliderList.children().eq(currentSlide).addClass('active');
-
-                    return slideShowMobile();
-                }
-
-                currentSlide++;
-                console.log(currentSlide);
-                $sliderList.children().eq(currentSlide).addClass('active').prev().addClass('hidden').removeClass('active').css({
-                    'width': 0,
-                    'margin': 0
-                });
-            }
         }
 
 });
