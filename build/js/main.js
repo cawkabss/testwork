6dc8694f1d503d8ce39626e5a2e7229b0df6c377
Aleksandr Poleliuk <cawkabss@gmail.com>
2016-09-08 17:38:09 +0300

    Refactoring JavaScript

--- build/js/main.js
+++ build/js/main.js
@@ -1,73 +1,98 @@
 $(document).ready(function () {
 
     var slider = {
-        autoPlay: '',
-        sliderItemWidth: '',
-        sliderItemMargin: '',
         $sliderList : $('.works-slider-list'),
+        $sliderItem : $('.works-slider-i'),
         $next : $('.slider-control-next'),
         $prev : $('.slider-control-prev'),
-        $sliderItem : $('.works-slider-i'),
-        currentSlide : 0,
-        sliderMargin: 0,
-        slideMargin: 0,
-        sum: 0,
         $sliderWidth : $('.works-slider').width(),
-        $sliderView : $('.works-slider')
-
+        $sliderView : $('.works-slider'),
+        currentSlide : 0,
+        sliderItemCss: {
+            width: 0,
+            margin: 0
+        },
+        autoPlay: false,
+        sliderInterval: '',
+        sliderMarginStep: 0,
+        sum: 0,
+        device: {
+            desktop: false,
+            tablet: false,
+            mobile: false
+        }
     };
+
+    function reset (){
+        slider.device = {
+            desktop: false,
+            tablet: false,
+            mobile: false
+        };
+        slider.currentSlide = 0;
+        slider.sliderItemCss = {
+            width: 0,
+            margin: 0
+        };
+        slider.sum = 0;
+        $.each(slider.$sliderItem, function(){
+            $(this).removeClass('hidden').removeClass('active');
+        });
+        slider.$sliderItem.first().addClass('active');
+        slider.$sliderList.css('margin', 0);
+    }
+
+    function responseSliderStyle (){
+        clearInterval(slider.sliderInterval);
+        if(slider.device.desktop){
+            slider.sliderItemCss.width = slider.$sliderWidth * 0.4 + 'px';
+            slider.sliderItemCss.margin = '0 ' + slider.$sliderWidth * 0.05 + 'px';
+            slider.$sliderItem.css(slider.sliderItemCss);
+            $(slider.$next).css('display', 'block');
+            $(slider.$prev).css('display', 'block');
+        }
+        else if(slider.device.tablet || slider.device.mobile){
+            slider.sliderInterval = setInterval(sliderAutoPlay, 2000);
+            slider.$next.css('display', 'none');
+            slider.$prev.css('display', 'none');
+
+            if(slider.device.tablet){
+                slider.sliderItemCss.width = slider.$sliderWidth * 0.6 + 'px';
+                slider.sliderItemCss.margin = '0 ' + slider.$sliderWidth * 0.1 + 'px';
+                slider.sliderMarginStep = - parseInt(slider.sliderItemCss.width) - slider.$sliderWidth * 0.1 * 2 ;
+
+                slider.$sliderItem.css(slider.sliderItemCss);
+            }
+            else if(slider.device.mobile){
+                slider.sliderItemCss.width = slider.$sliderWidth - 30 + 'px';
+                slider.sliderItemCss.margin = '0 ' + '15px';
+                slider.sliderMarginStep = - parseInt(slider.sliderItemCss.width) - 30 ;
+                slider.$sliderItem.css(slider.sliderItemCss);
+            }
+        }
+    }
 
     function response (){
         slider.$sliderList = $('.works-slider-list');
         slider.$next = $('.slider-control-next');
         slider.$prev = $('.slider-control-prev');
         slider.$sliderItem = $('.works-slider-i');
-        slider.currentSlide = 0;
         slider.$sliderWidth = $('.works-slider').width();
-        $.each(slider.$sliderItem, function(){
-            $(this).removeClass('hidden').removeClass('active');
-        });
-        slider.$sliderItem.first().addClass('active');
-        slider.$sliderList.css('margin', 0);
+
+        reset();
 
         if($(window).width() > 980){
-            clearInterval(slider.autoPlay);
-            slider.sliderItemWidth = slider.$sliderWidth * 0.4 + 'px';
-            slider.sliderItemMargin = '0 ' + slider.$sliderWidth * 0.05 + 'px';
-            slider.$sliderItem.css({
-                'width': slider.$sliderWidth * 0.4 + 'px',
-                'margin': '0 ' + slider.$sliderWidth * 0.05 + 'px'
-            });
-            $(slider.$next).css('display', 'block');
-            $(slider.$prev).css('display', 'block');
-
+            slider.device.desktop = true;
         }
         else if($(window).width() < 980){
-            clearInterval(slider.autoPlay);
-            slider.autoPlay = setInterval(slideShowTablet2, 2000);
-            slider.sum = 0;
-            slider.sliderItemWidth = slider.$sliderWidth * 0.6 + 'px';
-            slider.sliderItemMargin = '0 ' + slider.$sliderWidth * 0.1 + 'px';
-            slider.slideMargin = - slider.$sliderWidth * 0.6 - slider.$sliderWidth * 0.1 * 2 ;
-
-            slider.$sliderItem.css({
-                'width': slider.sliderItemWidth,
-                'margin': slider.sliderItemMargin
-            });
-            slider.$next.css('display', 'none');
-            slider.$prev.css('display', 'none');
+            slider.device.tablet = true;
 
             if($(window).width() < 750){
-                slider.sum = 0;
-                slider.sliderItemWidth = slider.$sliderWidth - 30 + 'px';
-                slider.sliderItemMargin = '0 ' + '15px';
-                slider.slideMargin = - slider.$sliderWidth;
-                slider.$sliderItem.css({
-                    'width': slider.sliderItemWidth,
-                    'margin': slider.sliderItemMargin
-                });
+                slider.device.mobile = true;
+                slider.device.tablet = false;
             }
         }
+        responseSliderStyle();
     }
 
     $(window).resize(function(){
@@ -85,8 +110,8 @@ $(document).ready(function () {
         slider.currentSlide++;
 
         slider.$sliderList.children().eq(slider.currentSlide).addClass('active').prev().addClass('hidden').removeClass('active');
-        slider.slideMargin = - slider.$sliderItem.eq(slider.currentSlide).width() - slider.$sliderWidth * 0.05 * 2;
-        slider.sum += slider.slideMargin;
+        slider.sliderMarginStep = - slider.$sliderItem.eq(slider.currentSlide).width() - slider.$sliderWidth * 0.05 * 2;
+        slider.sum += slider.sliderMarginStep;
         slider.$sliderList.css('margin-left', slider.sum + 'px')
 
     });
@@ -97,32 +122,21 @@ $(document).ready(function () {
         }
         slider.currentSlide--;
         slider.$sliderList.children().eq(slider.currentSlide).addClass('active').removeClass('hidden').next().removeClass('active');
-        slider.slideMargin = - slider.$sliderItem.eq(slider.currentSlide).width() - slider.$sliderWidth * 0.05 * 2;
-        slider.sum -= slider.slideMargin;
+        slider.sliderMarginStep = - slider.$sliderItem.eq(slider.currentSlide).width() - slider.$sliderWidth * 0.05 * 2;
+        slider.sum -= slider.sliderMarginStep;
         slider.$sliderList.css('margin-left', slider.sum + 'px')
 
     });
 
-        function slideShowTablet2() {
+        function sliderAutoPlay() {
             if(slider.currentSlide == slider.$sliderItem.length - 1){
-                $.each(slider.$sliderItem, function(){
-                    $(this).removeClass('hidden').removeClass('active');
-                });
-                slider.$sliderItem.css({
-                    'width': slider.sliderItemWidth,
-                    'margin': slider.sliderItemMargin
-                });
-                slider.sum = 0;
-                slider.sliderMargin = 0;
-                slider.$sliderList.css('margin-left', '0');
-                slider.currentSlide = 0;
-                slider.$sliderList.children().eq(slider.currentSlide).addClass('active');
+                reset();
                 return;
             }
             slider.currentSlide++;
             slider.$sliderList.children().eq(slider.currentSlide).addClass('active').prev().addClass('hidden').removeClass('active');
-            slider.sum += slider.slideMargin;
-            slider.$sliderList.css('margin-left', slider.sum + 'px')
+            slider.sum += slider.sliderMarginStep;
+            slider.$sliderList.css('margin-left', slider.sum + 'px');
         }
 
 });
